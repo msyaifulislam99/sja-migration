6c0373017828ff94b66bdc5f7f906c16f5ef5974
mohammad syaiful islam <msyaifulislam99@gmail.com>
2020-10-24 00:34:36 +0700

    pallet should be used

--- pallet_create_query.js
+++ pallet_create_query.js
@@ -7,6 +7,6 @@ const fs = require('fs');
 const file = fs.createWriteStream(`queries/pallet_query.txt`);
 for (const item of data || []) {
   file.write(
-    `insert into pallets ('number','weight', 'status') values ('${item.number}',${item.weight}, 'ready'); \n`
+    `insert into pallets ('number','weight', 'status') values ('${item.number}',${item.weight}, 'being_used'); \n`
   );
 }
